a91aff9ee8f68571816a7ac1f10be8292e08a986
Mehedi Hasan Topu <topu9872@gmail.com>
2026-05-21 23:28:39 +0600

    fix card image

--- src/app/all-cards/AllCards.jsx
+++ src/app/all-cards/AllCards.jsx
@@ -1,27 +1,35 @@
+"use client";
+
 import { Clock } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { FaEye } from "react-icons/fa";
 import { FaStar } from "react-icons/fa6";
+import { motion } from "framer-motion";
 
 const AllCards = ({ doctor }) => {
   return (
-    <div className="group relative bg-white rounded-3xl overflow-hidden border border-cyan-100 shadow-lg hover:shadow-cyan-300/40 hover:-translate-y-2 transition-all duration-300 w-90">
-      
+    <motion.div
+      initial={{ opacity: 0, y: 25 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5, ease: "easeOut" }}
+      viewport={{ once: true, amount: 0.2 }}
+      className="group relative bg-white rounded-3xl overflow-hidden border border-cyan-100 shadow-lg hover:shadow-cyan-300/40 hover:-translate-y-2 transition-all duration-300 w-90"
+    >
       {/* Rating */}
       <p className="absolute top-4 right-4 z-20 bg-white/90 backdrop-blur-md rounded-full px-3 py-1 font-semibold text-info flex items-center gap-1 shadow-md">
         <FaStar className="text-yellow-500" />
         {doctor.rating}
       </p>
 
-      {/* Image */}
-      <div className="overflow-hidden">
+      {/* IMAGE FIXED */}
+      <div className="relative w-full h-55 overflow-hidden">
         <Image
           src={doctor.image}
-          width={400}
-          height={240}
+          fill
           alt={doctor.name}
-          className="w-full h-60 object-cover group-hover:scale-105 transition-transform duration-500"
+          className="object-cover group-hover:scale-105 transition-transform duration-500"
+          sizes="(max-width: 768px) 100vw, 300px"
         />
       </div>
 
@@ -63,14 +71,14 @@ const AllCards = ({ doctor }) => {
         {/* Button */}
         <div className="pt-3">
           <Link href={`/all-cards/${doctor._id}`}>
-            <button className="btn btn-info w-full rounded-xl text-white text-lg font-semibold hover:scale-[1.02] transition-all duration-300">
+            <button className="btn btn-info w-full rounded-xl text-white text-lg font-semibold hover:scale-[1.02] transition-all duration-300 flex items-center justify-center gap-2">
               <FaEye />
               View Details
             </button>
           </Link>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
